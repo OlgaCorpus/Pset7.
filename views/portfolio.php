550b1f77c7b114ca071c3af112535a5f83fd83ce
Olga Corpus <olga1804@hotmail.com>
2016-03-18 20:43:16 +0000

    Añadir imagen a portfolio

--- views/portfolio.php
+++ views/portfolio.php
@@ -1,6 +1,7 @@
 <body style="background-color:white"/>
 <div class="jumbotron">
     <h1> Welcome, <?= $name["Name"]?><?= $name["Lastname"]?> </h1>
+     <img src="/img/manos.jpg"  width="500" height="300" /> 
 </div>
 <h1 style= "font-family: verdana; font-size: 50px; color: red;">Current cash: $<?= number_format($cash) ?>
 </h1>
